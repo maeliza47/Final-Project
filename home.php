39c2c681b355f17b03809c01faa25e829a176008
maeliza47 <33758120+maeliza47@users.noreply.github.com>
2017-11-29 15:36:43 -0500

    Create home.php

---
+++ home.php
@@ -0,0 +1,40 @@
+<?php
+include 'top.php';
+?>
+<!-- ----------------------------------------------------------------------- -->
+<article class="welcomeMessage">
+    <p class ="generalHello">
+        Marcelo y su amigo Chris están explorando un nuevo país en un carro 
+        alquilado. Ellos están buscando un restaurante llamado 'La mejor carne 
+        de la ciudad'. Sin embargo, están completamente perdidos. Las 
+        indicaciones que les dieron eran muy difíciles de seguir, por lo que
+        detienen a un desconocido al lado de la carretera para pedirle más 
+        indicaciones. El desconocido sonríe y responde: "Oh, ese es un 
+        restaurante muy popular. Les puedo decir cómo llegar allí. Es muy 
+        sencillo." Chris saca un papel y lápiz y comienza a tomar notas. El 
+        desconocido continúa.
+    </p>
+    
+    <p class="moreHello">
+        "Siga derecho hasta que vea una casa con una cerca rota. Gire a la 
+        derecha después de esa casa y siga hasta que pase a la izquierda dos 
+        grandes rocas. Poco después de eso usted verá un arbusto que parece una 
+        cucaracha gigante. Gire a la izquierda en ese arbusto y continúe durante 
+        otros veinte minutos. Debe pasar dos gasolineras a su derecha. Alrededor 
+        de una milla después de la segunda gasolinera, verá tres montículos de 
+        tierra y una casa con un caballo atado a un árbol. Gire a la derecha en 
+        esa casa y será el segundo restaurante a su izquierda. El letrero está 
+        roto pero es imposible que no lo vea. Tiene una roca al lado de él que 
+        se parece a un oso sosteniendo una espada. Es posible que vea algunas 
+        personas de pie afuera bebiendo cerveza. ¡Buena suerte!”
+    </p>
+    
+    <figure class="helloFigure">
+        <img class="helloImage" alt="" src="../finalProject/images/dandelion.jpg">
+    </figure>
+</article>
+<?php
+    include('footer.php');
+?>
+    </body>
+</html>
