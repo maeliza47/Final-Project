d779a26c6311d399fac3c49fcf65c5f7832fe042
maeliza47 <33758120+maeliza47@users.noreply.github.com>
2017-11-30 09:42:42 -0500

    Create gallery.php

---
+++ gallery.php
@@ -0,0 +1,26 @@
+<?php
+include 'top.php';
+?>
+<!-- ----------------------------------------------------------------------- -->
+<article class="galleryOfImages">
+    <figure class="helloFigure">
+        <img class="imageOne" alt="" src="../finalProject/images/dandelion.jpg">
+    </figure>
+    
+    <figure class="helloFigure">
+        <img class="imageTwo" alt="" src="../finalProject/images/dandelion.jpg">
+    </figure>
+    
+    <figure class="helloFigure">
+        <img class="imageThree" alt="" src="../finalProject/images/dandelion.jpg">
+    </figure>
+    
+    <figure class="helloFigure">
+        <img class="imageFour" alt="" src="../finalProject/images/dandelion.jpg">
+    </figure>
+</article>
+<?php
+    include('footer.php');
+?>
+    </body>
+</html>
