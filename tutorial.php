7d59485b0059d2a261f2c039b60329c78d5cb11c
maeliza47 <33758120+maeliza47@users.noreply.github.com>
2017-11-30 09:44:08 -0500

    Create tutorial.php

---
+++ tutorial.php
@@ -0,0 +1,21 @@
+<?php
+include 'top.php';
+?>
+<!-- ----------------------------------------------------------------------- -->
+<article class="tutorial">
+    <p class="tutoria11">
+        Yo también tengo una novia. Su nombre es Shenaaz. Ella se mudó con sus 
+        padres del Líbano a San Francisco. Ella es muy linda. Habla francés y 
+        árabe y está aprendiendo inglés. Ella todavía no tiene sus libros, 
+        entonces nos sentamos juntos en la clase y yo comparto mis libros con 
+        ella. El fin de semana, ella viene a mi casa y ve películas con mi 
+        familia y yo. A sus padres les gusta que ella pase tiempo en nuestra 
+        casa porque puede aprender inglés con nosotros. En casa, ellos sólo 
+        hablan francés o árabe.
+    </p>
+</article>
+<?php
+    include('footer.php');
+?>
+    </body>
+</html>
