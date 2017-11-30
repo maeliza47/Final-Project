ffa52c06d54f0c56e0f28abd685b9e7469952ae8
maeliza47 <33758120+maeliza47@users.noreply.github.com>
2017-11-30 09:42:18 -0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,36 @@
+<?php
+include 'top.php';
+?>
+<!-- ----------------------------------------------------------------------- -->
+<article class="about">
+    <p class="aboutOne">
+        Andrés no se siente bien. Estuvo en una fiesta anoche y llegó a casa a 
+        las 3 am. Tiene dolor de cabeza y siente náusea. Bebió mucho y comió 
+        demasiados camarones. Él se levanta de la cama y se da cuenta que no 
+        tiene puesto el pantalón. Mira alrededor pero no lo puede encontrar. 
+        Va al baño y busca sus pastas. No las puede encontrar. Él escucha voces.
+        Sale del dormitorio, baja por las escaleras y entra a la cocina. Hay 
+        gente hablando y riéndose en la cocina. Él se pregunta quién más volvió 
+        a casa con él anoche. Luego se da cuenta de algo. Él no tiene escaleras 
+        en su apartamento. ¡Está en la casa de otra persona!
+    </p>
+    <figure class="aboutFigure">
+        <img class="aboutImage" alt="" src="../finalProject/images/dandelion.jpg">
+    </figure>
+    <p class="aboutTwo">
+       "Veo un traumatismo en la cabeza y moretones por todo este cuerpo. No 
+       hay herida de bala. Tome una fotografía de la mesa del comedor. Alguien 
+       más estaba aquí... comiendo un sándwich... y no era la víctima. El 
+       asesino se comió el sándwich ya sea antes o después de darle una paliza 
+       a la víctima, posiblemente hasta matarla. De cualquier manera, esto no 
+       es un asesinato al azar; esto fue personal. Empecemos a localizar a 
+       familiares y amigos de la víctima. Tenemos que hablar con cualquiera 
+       que lo conocía. Alguien tiene que saber algo. ¿Escribiste todo eso? 
+       Empecemos con los vecinos". 
+    </p>
+</article>
+<?php
+    include('footer.php');
+?>
+    </body>
+</html>
