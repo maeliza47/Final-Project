592bc28e9e70ca084845d4e4c2c21bf4d9519a90
maeliza47 <33758120+maeliza47@users.noreply.github.com>
2017-12-05 12:08:38 -0500

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -4,23 +4,23 @@ include 'top.php';
 <!-- ----------------------------------------------------------------------- -->
 <article class="galleryOfImages">
     <figure class="helloFigure">
-        <img class="imageOne" alt="" src="../finalProject/images/dandelion.jpg">
+        <img class="imageOne" alt="" src="../finalProject/images/Image2.png">
     </figure>
     
     <figure class="helloFigure">
-        <img class="imageTwo" alt="" src="../finalProject/images/dandelion.jpg">
+        <img class="imageTwo" alt="" src="../finalProject/images/Image4.png">
     </figure>
     
     <figure class="helloFigure">
-        <img class="imageThree" alt="" src="../finalProject/images/dandelion.jpg">
+        <img class="imageThree" alt="" src="../finalProject/images/Image5.png">
     </figure>
     
     <figure class="helloFigure">
-        <img class="imageFour" alt="" src="../finalProject/images/dandelion.jpg">
+        <img class="imageFour" alt="" src="../finalProject/images/Image1.jpg">
     </figure>
-</article>
-<?php
+    <?php
     include('footer.php');
-?>
+    ?>
+</article>
     </body>
 </html>
