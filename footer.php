46c14fb5f60e5e0181699920baa0f5c309b609d2
maeliza47 <33758120+maeliza47@users.noreply.github.com>
2017-12-05 12:08:06 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,4 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<!-- ######################     Footer  #################################### -->
+<footer>
+	<p class="helpme">Designed by Mary Foran and Aaron Evans</p>
+</footer>
