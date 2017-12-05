ccc6a589e4aefa22c308dd37999238e5cc5a05bf
maeliza47 <33758120+maeliza47@users.noreply.github.com>
2017-12-05 12:09:16 -0500

    Update home.php

--- home.php
+++ home.php
@@ -2,39 +2,56 @@
 include 'top.php';
 ?>
 <!-- ----------------------------------------------------------------------- -->
-<article class="welcomeMessage">
-    <p class ="generalHello">
-        Marcelo y su amigo Chris están explorando un nuevo país en un carro 
-        alquilado. Ellos están buscando un restaurante llamado 'La mejor carne 
-        de la ciudad'. Sin embargo, están completamente perdidos. Las 
-        indicaciones que les dieron eran muy difíciles de seguir, por lo que
-        detienen a un desconocido al lado de la carretera para pedirle más 
-        indicaciones. El desconocido sonríe y responde: "Oh, ese es un 
-        restaurante muy popular. Les puedo decir cómo llegar allí. Es muy 
-        sencillo." Chris saca un papel y lápiz y comienza a tomar notas. El 
-        desconocido continúa.
+<article class="homepage">
+    <p class ="welcome">
+      Hello and welcome to our website. The purpose of this website is to 
+      provide an educational and instructional experience into the basics of 
+      astrophotography. On the various pages you will see as you explore the 
+      site you will find pages to learn, pages to admire, and more.  
     </p>
     
-    <p class="moreHello">
-        "Siga derecho hasta que vea una casa con una cerca rota. Gire a la 
-        derecha después de esa casa y siga hasta que pase a la izquierda dos 
-        grandes rocas. Poco después de eso usted verá un arbusto que parece una 
-        cucaracha gigante. Gire a la izquierda en ese arbusto y continúe durante 
-        otros veinte minutos. Debe pasar dos gasolineras a su derecha. Alrededor 
-        de una milla después de la segunda gasolinera, verá tres montículos de 
-        tierra y una casa con un caballo atado a un árbol. Gire a la derecha en 
-        esa casa y será el segundo restaurante a su izquierda. El letrero está 
-        roto pero es imposible que no lo vea. Tiene una roca al lado de él que 
-        se parece a un oso sosteniendo una espada. Es posible que vea algunas 
-        personas de pie afuera bebiendo cerveza. ¡Buena suerte!”
+    <p class="welcomeIntro">
+      On the Introduction page, you will find a brief introduction as to what 
+      exactly astrophotography is. The page goes over the basics as to how 
+      astrophotography works, and what it is used for in the world.  
     </p>
     
-    <figure class="helloFigure">
-        <img class="helloImage" alt="" src="../finalProject/images/dandelion.jpg">
+    <p class="welcomeTutorial">
+      On the Tutorial page, you will find a step-by-step procedure on how to 
+      use ImageJ to process images on your own!  
+    </p>
+    
+    <p class="welcomeAbout">
+        On the About page, you will find a brief description as to what the 
+        ImageJ program is. The page covers only the basics, but a link to the 
+        main website is provided for those interested.
+    </p>
+    
+    <p class="welcomeForm">
+       Lastly, there is the Form page. On this page you will find a survey that 
+       we have created to get a general idea of peoples’ awareness of the 
+       astronomy world. It contains a few basic questions as to what you know, 
+       what you have heard of, etc. We ask that you consider taking the time to 
+       check it out, for it will add to our data. Thank you! 
+    </p>
+    
+    <p class="welcomeGallery">
+       There is also a gallery for your viewing pleasure. It currently contains 
+       only a few examples of what astrophotography can do, but we have hope to 
+       get some from the community to display in the future. 
+    </p>
+    
+    <figure class="welcomeImage">
+        <img class="helloImage" alt="" src="../finalProject/images/Image1.jpg">
     </figure>
-</article>
-<?php
+    
+    <div id="bg">
+        <img src="../finalProject/images/back.png">
+    </div>
+    
+    <?php
     include('footer.php');
-?>
+    ?>
+</article>
     </body>
 </html>
